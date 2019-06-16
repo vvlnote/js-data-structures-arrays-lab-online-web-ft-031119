110b8c23a7024390eeb85773d7c8e14d321579d0
Joy Lin <vvlnote@hotmail.com>
2019-06-16 21:36:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ let drivers = ["Milo", "Otis", "Garfield"];
 function destructivelyAppendDriver(name) {
   return drivers.push(name);
 }
+
+function destructivelyPrependDriver(name) {
+  return drivers.unshift(name);
+}
